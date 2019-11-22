58e62da5e283d1517ab4d3525a6fde5c6169c405
yuiriizubkov <zubkovyuriy@gmail.com>
2019-11-22 14:34:36 +0100

    socket.io setup

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,70 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app clipped>
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-calendar-clock</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Schedule</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-timeline-alert</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Events</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-settings</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Settings</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app clipped-left>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Smart Pet Feeder</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col class="shrink">12345</v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+
+    <v-footer app>
+      <span>{{connectionStateString}}</span>
+    </v-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import { mapState } from 'vuex'
+export default {
+  data: () => ({
+    drawer: null,
+  }),
+  computed: mapState(['connectionStateString']),
+  created() {
+    this.$vuetify.theme.dark = true
+  },
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,61 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
+import io from 'socket.io-client'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+const store = new Vuex.Store({
+  state: {
+    connected: false,
+    connectionStateString: 'Connecting...',
+  },
+  mutations: {
+    connectionState(state, isConnected) {
+      state.connected = isConnected
+    },
+    connectionStateStr(state, str) {
+      state.connectionStateString = str
+    },
+  },
+  actions: {
+    // connect(context) {},
+  },
+  modules: {},
+})
+
+const socket = io('http://192.168.8.106:65500', {
+  path: '/api',
+  transports: ['websocket'],
+})
+
+socket.on('connect', () => {
+  store.commit('connected', true)
+  store.commit('connectionStateStr', 'Connected')
+  console.info('Connection state:', 'Connected')
+})
+
+socket.on('disconnect', () => {
+  store.commit('connected', false)
+  store.commit('connectionStateStr', 'Disconnected')
+  console.info('Connection state:', 'Disconnected')
+})
+
+socket.on('reconnecting', n => {
+  store.commit('connected', false)
+  store.commit('connectionStateStr', `Trying to reconnect (${n})`)
+  console.info('Connection state:', 'Reconnecting...', n)
+})
+
+socket.on('reconnect_failed', () => {
+  store.commit('connected', false)
+  store.commit('connectionStateStr', `Reconnection failed`)
+  console.error('Connection state:', 'Reconnection failed')
+})
+
+socket.on('connect_error', err => {
+  store.commit('connected', false)
+  store.commit('connectionStateStr', `Connection error: ${err.message}`)
+  console.error(err)
+})
+
+export default store
