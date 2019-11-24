ac0bfb71c3dc251d16f2728e813d5acb73d006e2
yuiriizubkov <zubkovyuriy@gmail.com>
2019-11-24 11:41:13 +0100

    loading

--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -1,5 +1,10 @@
 <template>
-  <v-container class="fill-height" fluid>
+  <v-container fluid>
+    <v-row v-if="schedule && schedule.length === 0" align="center" justify="center">
+      <v-col cols="1">
+        <v-progress-circular :size="70" :width="7" color="purple" indeterminate />
+      </v-col>
+    </v-row>
     <v-row align="center" justify="center">
       <v-col cols="12" sm="8" md="8">
         <ScheduleEntry
