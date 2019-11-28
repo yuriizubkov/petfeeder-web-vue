60a16018817cef0f265d252e3b938702f85ec85b
yuiriizubkov <zubkovyuriy@gmail.com>
2019-11-28 13:51:02 +0100

    local icons and font

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,12 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
+import 'typeface-roboto/index.css'
+import '@mdi/font/css/materialdesignicons.css'
 
-Vue.use(Vuetify);
+Vue.use(Vuetify)
 
 export default new Vuetify({
-});
+  icons: {
+    iconfont: 'mdi',
+  },
+})
